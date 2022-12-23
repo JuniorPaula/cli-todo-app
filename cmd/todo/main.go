79524038b2ecbeb74d7afda35ca765d499b15961
Junior Paula <luke.junnior@icloud.com>
2022-12-23 08:37:02 -0300

    Implements method to add a new task

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+	todogocli "todocli"
+)
+
+const (
+	todoFile = "./todos.json"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+
+	add := flag.Bool("add", false, "add a new todo")
+
+	flag.Parse()
+
+	todos := &todogocli.Todos{}
+
+	if err := todos.Load(todoFile); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+
+	switch {
+	case *add:
+		todos.Add("Sample todo")
+		err := todos.Store(todoFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+	default:
+		fmt.Fprintln(os.Stdout, "invalid command❕")
+		os.Exit(1)
+	}
 }
